17a79fe6c648632270a4022437415f51bad13ff2
Thomas Lété <bistory@gmail.com>
2011-07-07 06:57:15 -0700

    Doesn't render the toolbar if auto_render is set to false.

--- init.php
+++ init.php
@@ -5,4 +5,5 @@ if(Kohana::config('debug_toolbar.firephp_enabled') === TRUE)
 	require_once Kohana::find_file('vendor', 'firephp/packages/core/lib/FirePHPCore/FirePHP.class');
 
 // Render Debug Toolbar on the end of application execution
-register_shutdown_function('debugtoolbar::render');
+if(Kohana::config('debug_toolbar.auto_render') === TRUE)
+	register_shutdown_function('debugtoolbar::render');
