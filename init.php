d9e48a47c39b99fda5e34fa2c8a771687efed4b7
Maxim <Big_Shark@mail.ru>
2011-07-28 23:11:41 +1100

    update 3.2 version

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 // Load FirePHP if it enabled in config
-if(Kohana::config('debug_toolbar.firephp_enabled') === TRUE)
+if(Kohana::$config->load('debug_toolbar.firephp_enabled') === TRUE)
 	require_once Kohana::find_file('vendor', 'firephp/packages/core/lib/FirePHPCore/FirePHP.class');
 
 // Render Debug Toolbar on the end of application execution
